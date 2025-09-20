e3d2eefe74c5632396a8f11de47c37648a842e4c
Túlio Dias <57201326+TTDias@users.noreply.github.com>
2025-09-20 11:10:05 -0300

    Adding cat and cat behavior

---
+++ TI3 - Vet/Assets/Script/Cat/CatPlay.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CatPlay : MonoBehaviour
+{
+
+    public float waitTime;
+    float cooldown;
+    bool waiting;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldown > 0 && waiting) cooldown -= Time.deltaTime;
+        if (cooldown <=0 && waiting)
+        {
+            waiting = false;
+            Runaway();
+        }
+    }
+
+    public void Call()
+    {
+        cooldown = waitTime;
+        waiting = true;
+    }
+
+    public void Sleep()
+    {
+        GetComponent<CatMove>().Sleep();
+    }
+
+    public void Cancel()
+    {
+        waiting = false;
+    }
+
+    void Runaway()
+    {
+        GetComponent<CatMove>().Runaway();
+    }
+}
