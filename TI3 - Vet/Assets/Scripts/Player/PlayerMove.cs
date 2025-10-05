96edb9b313d61a61d52bf137791660dd710c2b65
Túlio Dias <57201326+TTDias@users.noreply.github.com>
2025-10-04 23:11:11 -0300

    Test update new input system

--- TI3 - Vet/Assets/Scripts/Player/PlayerMove.cs
+++ TI3 - Vet/Assets/Scripts/Player/PlayerMove.cs
@@ -1,27 +1,57 @@
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
     public NavMeshAgent agent;
 
-    void Update()
-    {
-        if (Input.GetMouseButton(0) || (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Stationary || Input.GetTouch(0).phase == TouchPhase.Moved)))
-        {
+    //void Update()
+    //{
+    //    if (UIRepairScript.Instance.reparing == true) return;
+        
+    //    bool mouseClick = Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject();
+    //    bool touchClick = Input.touchCount > 0 
+    //                      && (Input.GetTouch(0).phase == TouchPhase.Stationary || Input.GetTouch(0).phase == TouchPhase.Moved) 
+    //                      && EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                agent.SetDestination(hit.point);
-            }
-        }
-        else if(Input.GetMouseButtonUp(0) || Input.touchCount == 0)
+    //    if(Input.GetMouseButtonUp(0) || Input.touchCount > 0)
+    //    {
+    //        Debug.Log("Mouse na ui: " + EventSystem.current.IsPointerOverGameObject());
+    //        Debug.Log("Touch na ui: " + EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId));
+    //    }
+
+    //    if (mouseClick || touchClick)
+    //    {
+    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //        RaycastHit hit;
+    //        if (Physics.Raycast(ray, out hit))
+    //        {
+    //            agent.SetDestination(hit.point);
+    //        }
+    //    }
+    //    else if(Input.GetMouseButtonUp(0) || Input.touchCount == 0)
+    //    {
+    //        agent.ResetPath();
+    //    }
+    //}
+
+    public void OnClickOrTouch(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+
+        // Verifica se o clique está sobre a UI
+        //if (IsPointerOverUI()) return;
+
+        // Raycast para detectar ponto de destino
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
         {
-            agent.ResetPath();
+            agent.SetDestination(hit.point);
         }
     }
+
 
 }
--- TI3 - Vet/Assets/Scripts/UIRepairScript.cs
+++ TI3 - Vet/Assets/Scripts/UIRepairScript.cs
@@ -4,8 +4,10 @@ using UnityEngine.UI;
 public class UIRepairScript : MonoBehaviour
 {
     public static UIRepairScript Instance;
-    public static Button btn;
+    public Button btn;
+    public Image loadBar;
     CatItens obj;
+    public bool reparing = false;
 
     private void Awake()
     {
@@ -26,13 +28,13 @@ public class UIRepairScript : MonoBehaviour
         btn.interactable = true;
         obj = item;
         btn.onClick.AddListener(obj.Repair);
-        btn.onClick.AddListener( () => { btn.interactable = false; });
+        btn.onClick.AddListener( ButtonLoad );
     }
 
     public void Deselect()
     {
-        //if(btn.onClick.GetPersistentEventCount() > 0)
-        //    btn.onClick.RemoveAllListeners();
+        if (btn.onClick.GetPersistentEventCount() > 0)
+            btn.onClick.RemoveAllListeners();
         obj.Unfocus();
         btn.interactable = false;
     }
@@ -40,5 +42,20 @@ public class UIRepairScript : MonoBehaviour
     public void TestEvent()
     {
         Debug.Log("Mensagem");
+    }
+
+    void ButtonLoad()
+    {
+        reparing = true;
+        loadBar.gameObject.SetActive(true);
+        LeanTween.value(gameObject, 0, 1, 3)
+            .setOnUpdate((float val) => { loadBar.fillAmount = val; })
+            .setOnComplete(() => {
+                reparing = false;
+                LeanTween.delayedCall(gameObject, 1, () => { 
+                    loadBar.gameObject.SetActive(false); 
+                    btn.interactable = false; 
+                }); 
+            });
     }
 }
