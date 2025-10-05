8acd019d6ca01e1c2c3a334b5b1907dc1a3d995a
Túlio Dias <57201326+TTDias@users.noreply.github.com>
2025-10-05 00:22:41 -0300

    Fixing new input system

--- TI3 - Vet/Assets/Scripts/Player/PlayerMove.cs
+++ TI3 - Vet/Assets/Scripts/Player/PlayerMove.cs
@@ -2,56 +2,54 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerMove : MonoBehaviour
 {
     public NavMeshAgent agent;
+    bool isPressed;
+    Vector2 position;
 
-    //void Update()
-    //{
-    //    if (UIRepairScript.Instance.reparing == true) return;
-        
-    //    bool mouseClick = Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject();
-    //    bool touchClick = Input.touchCount > 0 
-    //                      && (Input.GetTouch(0).phase == TouchPhase.Stationary || Input.GetTouch(0).phase == TouchPhase.Moved) 
-    //                      && EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
-
-    //    if(Input.GetMouseButtonUp(0) || Input.touchCount > 0)
-    //    {
-    //        Debug.Log("Mouse na ui: " + EventSystem.current.IsPointerOverGameObject());
-    //        Debug.Log("Touch na ui: " + EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId));
-    //    }
-
-    //    if (mouseClick || touchClick)
-    //    {
-    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    //        RaycastHit hit;
-    //        if (Physics.Raycast(ray, out hit))
-    //        {
-    //            agent.SetDestination(hit.point);
-    //        }
-    //    }
-    //    else if(Input.GetMouseButtonUp(0) || Input.touchCount == 0)
-    //    {
-    //        agent.ResetPath();
-    //    }
-    //}
-
-    public void OnClickOrTouch(InputAction.CallbackContext context)
+    void Update()
     {
-        if (!context.performed) return;
+        if (UIRepairScript.Instance.reparing == true) return;
 
-        // Verifica se o clique está sobre a UI
-        //if (IsPointerOverUI()) return;
+        //    bool mouseClick = Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject();
+        //    bool touchClick = Input.touchCount > 0 
+        //                      && (Input.GetTouch(0).phase == TouchPhase.Stationary || Input.GetTouch(0).phase == TouchPhase.Moved) 
+        //                      && EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
 
-        // Raycast para detectar ponto de destino
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        //    if(Input.GetMouseButtonUp(0) || Input.touchCount > 0)
+        //    {
+        //        Debug.Log("Mouse na ui: " + EventSystem.current.IsPointerOverGameObject());
+        //        Debug.Log("Touch na ui: " + EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId));
+        //    }
+
+        if (isPressed)
         {
-            agent.SetDestination(hit.point);
+            Ray ray = Camera.main.ScreenPointToRay(position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
         }
+        else if (agent.hasPath)
+        {
+            agent.ResetPath();
+        }
+    }
+
+    public void OnPointerPosition(InputAction.CallbackContext context)
+    {
+        position = context.ReadValue<Vector2>();
+    }
+
+    public void OnPointerClick(InputAction.CallbackContext context)
+    {
+        isPressed = context.ReadValue<float>() > 0;
     }
 
 
 }
+
