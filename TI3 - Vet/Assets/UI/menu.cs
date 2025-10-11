998377df95fe8a6411e072d98c23958c827bc55f
PCnorby22 <bigleite10@gmail.com>
2025-10-11 14:16:56 -0300

    menuinicial pronto
    Gustavo

--- TI3 - Vet/Assets/UI/menu.cs
+++ TI3 - Vet/Assets/UI/menu.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    public GameObject[] obj;
+    public GameObject[] obj, slaider;
     float t = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -38,10 +39,86 @@ public class menu : MonoBehaviour
     }
     public void botaojogar()
     {
-       
+        SceneManager.LoadScene("TestMechanics");
     }
     public void botaocreditos()
     {
-
+        SceneManager.LoadScene("Creditos");
+    }
+    public void botaoOpçoes()
+    {
+        LeanTween.cancelAll();
+        LeanTween.init();
+        t = 0;
+        foreach (GameObject G in obj)
+        {
+            if (G.name != "menu")
+            {
+                LeanTween.scale(G, new Vector3(0f, 0f, 0f), 0.5f).setDelay(t).setOnComplete(() =>
+                {
+                    t = 0;
+                    foreach (GameObject g in obj)
+                    {
+                        if (g.name != "menu")
+                        {
+                            g.SetActive(false);
+                        }
+                    }
+                    foreach (GameObject S in slaider)
+                    {
+                        S.gameObject.SetActive(true);
+                    }
+                    foreach (GameObject S in slaider)
+                    {
+                        LeanTween.scale(S, new Vector3(1f, 1f, 1f), 0.5f).setDelay(t);
+                        t += 0.2f;
+                    }
+                });
+                
+            }
+        }
+        
+    }
+    public void botaoVoltarMenu()
+    {
+        LeanTween.cancelAll();
+        LeanTween.init();
+        t = 0;
+        foreach (GameObject S in slaider)
+        {
+            LeanTween.scale(S, new Vector3(0f, 0f, 0f), 0.5f).setDelay(t).setOnComplete(() =>
+            {
+                //Debug.Log(t);
+                foreach (GameObject S in slaider)
+                {
+                    S.gameObject.SetActive(false);
+                }
+                foreach (GameObject g in obj)
+                {
+                    if (g.name != "menu")
+                    {
+                        g.SetActive(true);
+                    }
+                }
+                t = 0;
+                foreach (GameObject G in obj)
+                {
+                    LeanTween.scale(G, new Vector3(1, 1, 1), 0.5f).setDelay(t);
+                    t += 0.5f;
+                }
+                foreach (GameObject G in obj)
+                {
+                    LeanTween.scale(G, new Vector3(1.1f, 1.1f, 1.1f), 0.5f).setOnComplete(() =>
+                    {
+                        if (G.name != "menu")
+                        {
+                            //t = 0;
+                            LeanTween.scale(G, new Vector3(1, 1, 1), 0.5f).setLoopPingPong();
+                            //t += 10;
+                        }
+                    });
+                }
+            });
+        }
     }
 }
